9bbc642a6562f714aa0b00f99f2e1255198ecb28
unknown <idan8ezer@gmail.com>
2024-01-04 16:50:15 +0200

    fixed the gradle implementation to fix the IR Lowering bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.7"
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -65,7 +74,7 @@ dependencies {
     // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
+//    kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Retrofit
@@ -73,8 +82,4 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
-}
-
-kapt {
-    correctErrorTypes = true
 }
